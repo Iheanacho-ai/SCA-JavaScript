3b3ac1ff5f18ecf91b2799f30de0a9d2d4acb23b
Iheanacho Amarachi Sharon <58210919+Iheanacho-ai@users.noreply.github.com>
2020-06-16 09:47:19 +0100

    update main.js
    I changed the syntax from ES5 to ES6

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Finds out which number is larger.
-function large(a, b){
+const large = (a, b) => {
    if (a > b) {
        console.log(a)
    } else if(b > a){
@@ -9,25 +9,24 @@ function large(a, b){
    )
 }
 
-large(3, 5)
 
 
 // Finds out which number out of 20 is odd or even
 for (let i = 0; i <= 20; i++) {
     if (i %2 === 0) {
-        console.log(i + " is even")
+        console.log(`${i} is even`)
     } else {
-        console.log(i + " is odd")
+        console.log(`${i} is odd`)
     }
 }
 
 // Finds out which number out of 100 is a multiple of 3 or 5 or both
 for (let i = 0; i <= 100; i++) {
     if(i % 3 === 0 && i % 5 === 0){
-        console.log(i + " FizzBuzz")
+        console.log(`${i} FizzBuzz`)
     } else if (i % 3 === 0 ) {
-        console.log(i + " Fizz") 
+        console.log(`${i} Fizz`) 
      } else if(i % 5 === 0) {
-        console.log(i + " Buzz") 
+        console.log(`${i} Buzz`) 
      }
 }
